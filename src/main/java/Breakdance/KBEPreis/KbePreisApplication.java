254fe147f0111f7b2ba1c9cc218ad9b4a715aea6
Thien Duong <duongthien1809@gmail.com>
2023-02-03 22:19:34 +0100

    first commit

---
+++ src/main/java/Breakdance/KBEPreis/KbePreisApplication.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEPreis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbePreisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbePreisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Breakdance/KBEPreis/KbePreisApplicationTests.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEPreis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbePreisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
